6542af7eeb977d14287f4502f46c7a92b4fa3c46
Aryan99297 <aryandbg99@gmail.com>
2025-09-03 21:09:42 +0000

    conversion

---
+++ basics/conversionOp.js
@@ -0,0 +1,13 @@
+//33abc NaN
+//true =1
+let isLoggedIn =1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)   //true
+//emptystring=false
+//hitesh=true
+
+
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(stringNumber)  //33
+console.log(typeof stringNumber) //string
