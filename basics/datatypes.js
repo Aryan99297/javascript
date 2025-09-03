d26e104141e3eb4e9b9762eb81b13d7d2ca52f26
Aryan99297 <aryandbg99@gmail.com>
2025-09-03 20:54:32 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,10 @@
+/*null
+undefined
+Symbolobject
+Boolean
+Numberstring
+*/
+let age =23
+console.log(typeof age) //number
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
---
+++ basics/variable.js
@@ -0,0 +1,8 @@
+const accountId =14433
+let accountEmail="aryan@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState
+//accountId=12 not allowed to chane const
+console.log(accountId)
+console.table([accountEmail,accountId,accountPassword,accountState])
