de2bc069d6d385690facc738dc8aab4a7bd99357
Aryan99297 <aryandbg99@gmail.com>
2025-09-03 21:57:41 +0000

    summary

---
+++ basics/datatypesSummary.js
@@ -0,0 +1,19 @@
+//primitive num strng bool ,null ,undefined,symbol,bigInt
+//non primitive array, fn, obj
+let id=Symbol('123')
+let anotherid=Symbol('123')
+console.log(id===anotherid)   //false
+const bigNumber=37337373787384878497783n
+//primitive typeof values : 
+// Number => number
+// String => string
+// Boolean => boolean
+// null => object
+// Undefined => undefined
+// Symbol => symbol
+// BigInt => bigint
+
+//for non prem 
+// array => object.
+// object => object.
+// function => function. // said as : (function object)
