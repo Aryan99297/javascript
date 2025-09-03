458334f95a0191357d11a2d7427372ec234ea361
Aryan99297 <aryandbg99@gmail.com>
2025-09-03 23:36:18 +0000

    math

---
+++ basics/math.js
@@ -0,0 +1,13 @@
+const min=1
+const max=6
+console.log(Math.ceil(Math.random()*(max-min+1))) +min
+//dice
+
+
+
+
+
+// number between 10 and 50
+const miin=10
+const maxx=50
+console.log(Math.floor(Math.random()*(maxx-miin)+1)+miin)
