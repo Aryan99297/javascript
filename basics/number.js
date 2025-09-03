c6eefbbc09e611349f1857b217bf78631b637a8a
Aryan99297 <aryandbg99@gmail.com>
2025-09-03 23:26:10 +0000

    number

---
+++ basics/number.js
@@ -0,0 +1,10 @@
+const score=300
+console.log(score)
+const balance =new Number(1000000)
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+const other =123.8966
+console.log(other.toPrecision(4));
+const hundreds =1000000
+console.log(hundreds.toLocaleString('en-IN'))
