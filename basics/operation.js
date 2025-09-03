ef0e0dbb8fe74467e0c8e95ac68f4e516a7eb50b
Aryan99297 <aryandbg99@gmail.com>
2025-09-03 21:26:46 +0000

    operation

---
+++ basics/operation.js
@@ -0,0 +1,24 @@
+/*let value =3
+let negValue=-value
+console.log(negValue)
+console.log(2**3)
+
+
+let str1="hello"
+let str2=" aryan"
+let str3=str1+str2
+console.log(str3)
+*/
+console.log(+true)//1
+console.log(+"") //0
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
