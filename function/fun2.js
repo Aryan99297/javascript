0e5a1f033327c7147ffcdab290ff08f8114c494c
Aryan99297 <aryandbg99@gmail.com>
2025-09-04 15:54:59 +0000

    funn2

---
+++ function/fun2.js
@@ -0,0 +1,21 @@
+/*
+
+const user ={
+    username:"hitesh",
+    price:199
+}
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and the price is ${anyobject.price}`)
+}
+return handleObject(
+    {
+        username:"aryan",
+        price:300000
+    }
+)*/
+
+const myNewArray=[200,400,100,600]
+function returnSecondArray(getArray){
+    return getArray[1]
+}
+console.log(returnSecondArray([1,2,3,4]))
