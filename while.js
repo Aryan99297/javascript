9bbc77430f3d12b40780224d8524ed69640db558
Aryan99297 <aryandbg99@gmail.com>
2025-09-05 14:24:51 +0000

    while

---
+++ while.js
@@ -0,0 +1,12 @@
+let myArray=['batman','superman','perman']
+let i=0
+while(i<myArray.length){
+    console.log(`valuse of array is  ${myArray[i]}`)
+    i++
+}
+
+let score=11
+do {
+    console.log(`value of score is ${score}`)
+    score++
+}while(score<=10);
